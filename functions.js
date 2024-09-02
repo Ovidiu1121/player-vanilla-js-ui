332c303eecf756b6260a8d0c3c2f791356242993
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-09-02 16:22:10 +0300

    done alerts

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
-export function createHome() {
+export function createHome(alert) {
 
     let container = document.querySelector(".container");
 
 
     container.innerHTML = `
-    
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+       </div>  
+
     	<h1>Players</h1>
 
     <button class="button">Add player</button>
@@ -22,22 +25,76 @@ export function createHome() {
 		</tbody>
 	</table>
     `
+    let button = document.querySelector(".button");
+    let table = document.querySelector(".table");
+    const alertPlaceholder = document.querySelector('.container-alert');
+    let load = document.querySelector(".spinner-border");
+
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
 
     api("https://localhost:7035/api/v1/Player/all").then(response => {
         return response.json();
     }).then(data => {
+        load.classList = "";
         console.log(data);
         attachPlayers(data.playerList);
     }).catch(error => {
+        load.classList = "";
         console.error('Error fetching data:', error);
+        appendAlert(error, "danger");
     });
-
-
-    let button = document.querySelector(".button");
 
     button.addEventListener("click", (eve) => {
         CreateAddPlayerPage();
     });
+
+    table.addEventListener("click", (eve) => {
+
+        if (eve.target.classList.contains("updatePlayer")) {
+            api(`https://localhost:7035/api/v1/Player/id/${eve.target.textContent}`).then(res => {
+                return res.json();
+            }).then(data => {
+                console.log(data);
+
+                let player = {
+                    name: data.name,
+                    number: data.number,
+                    value: data.value
+                }
+
+                CreateUpdatePage(player, eve.target.textContent);
+
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+            });
+        }
+
+    });
+
+    if (alert === "deleted") {
+        load.classList = "";
+        appendAlert("Player has been DELETED with success!", "success");
+    }
+
+    if (alert === "updated") {
+        load.classList = "";
+        appendAlert("Player has been UPDATED with success!", "success");
+    }
+
+    if (alert === "added") {
+        load.classList = "";
+        appendAlert("Player has been ADDED with success!", "success");
+    }
 
 }
 
@@ -79,12 +136,83 @@ export function CreateAddPlayerPage() {
     let test = document.querySelector(".createPlayer");
 
     button.addEventListener("click", (eve) => {
-        createHome();
+        createHome("");
     })
 
     test.addEventListener("click", (eve) => {
-        createPlayer();
+        createUpdatePlayer("create");
     })
+
+}
+
+export function CreateUpdatePage(player, idPlayer) {
+
+    let container = document.querySelector(".container");
+
+    container.innerHTML = `
+    <h1>Update Player</h1>
+    <form>
+        <p>
+            <label for="name">Name</label>
+            <input name="name" type="text" id="name" value="${player.name}">
+             <a class="nameErr">Name required!</a>
+        </p>
+        <p>
+            <label for="number">Number</label>
+            <input name="number" type="text" id="number" value="${player.number}">
+             <a class="numberErr">Number required!</a>
+        </p>
+        <p>
+            <label for="value">Value</label>
+            <input name="value" type="text" id="value" value="${player.value}">
+             <a class="valueErr">Value required!</a>
+        </p>
+
+        <div class="submitUpdate">
+         <a href="#">Update Player</a>
+        </div>
+
+          <div class="cancel">
+         <a href="#">Cancel</a>
+        </div>
+        <div class="submitDelete">
+         <a href="#">Delete Player</a>
+        </div>
+    </form>
+    `
+
+    let cancelButton = document.querySelector(".cancel");
+    let submitUpdateButton = document.querySelector(".submitUpdate");
+    let submitDeleteButton = document.querySelector(".submitDelete");
+    let nameinput = document.getElementById("name");
+
+    nameinput.disabled = true;
+
+    cancelButton.addEventListener("click", (eve) => {
+        createHome("");
+    });
+
+    submitUpdateButton.addEventListener("click", (eve) => {
+        createUpdatePlayer("update", idPlayer);
+    });
+
+    submitDeleteButton.addEventListener("click", (eve) => {
+
+        api(`https://localhost:7035/api/v1/Player/delete/${idPlayer}`, "DELETE")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+
+                createHome("deleted");
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+    })
+
 
 }
 
@@ -93,7 +221,7 @@ function createRow(player) {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-				<td>${player.id}</td>
+				<td class="updatePlayer">${player.id}</td>
 				<td>${player.name}</td>
 				<td>${player.number}</td>
 				<td>${player.value}</td>
@@ -134,7 +262,7 @@ function attachPlayers(players) {
 
 }
 
-function createPlayer() {
+function createUpdatePlayer(request, idPlayer) {
 
     const isNumber = (str) => {
         return /^[+-]?\d+(\.\d+)?$/.test(str);
@@ -217,17 +345,31 @@ function createPlayer() {
             value: valueInput
         }
 
-        api("https://localhost:7035/api/v1/Player/create", "POST", player)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                createHome();
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        if (request === "create") {
+            api("https://localhost:7035/api/v1/Player/create", "POST", player)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome();
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else if (request === "update") {
+            api(`https://localhost:7035/api/v1/Player/update/${idPlayer}`, "PUT", player)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("updated");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     } else {
 
         errors.forEach(err => {
